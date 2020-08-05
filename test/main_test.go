c4af9220df3b36359622505b1c450de5221e7e93
Tomoya Nishide <tom.24d.en@gmail.com>
2020-08-05 19:13:17 +0900

    add test main for channel config, ran update deps

--- test/main_test.go
+++ test/main_test.go
@@ -1,1 +1,23 @@
 package test
+
+import (
+	"testing"
+	"os"
+
+	"knative.dev/eventing/test"
+	testlib "knative.dev/eventing/test/lib"
+)
+
+var channelTestRunner testlib.ComponentsTestRunner
+var brokerClass string
+
+func TestMain(m *testing.M) {
+	test.InitializeEventingFlags()
+	channelTestRunner = testlib.ComponentsTestRunner{
+		ComponentFeatureMap: testlib.ChannelFeatureMap,
+		ComponentsToTest:    test.EventingFlags.Channels,
+	}
+	brokerClass = test.EventingFlags.BrokerClass
+
+	os.Exit(m.Run())
+}
